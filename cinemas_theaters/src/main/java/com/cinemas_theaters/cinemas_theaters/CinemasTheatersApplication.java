0ce6ec2d3ece754990676885dd18fe20bfe173e2
Filip <basarafilip@gmail.com>
2018-04-23 18:11:23 +0200

    initial project

---
+++ cinemas_theaters/src/main/java/com/cinemas_theaters/cinemas_theaters/CinemasTheatersApplication.java
@@ -0,0 +1,12 @@
+package com.cinemas_theaters.cinemas_theaters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemasTheatersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemasTheatersApplication.class, args);
+	}
+}
---
+++ cinemas_theaters/src/test/java/com/cinemas_theaters/cinemas_theaters/CinemasTheatersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cinemas_theaters.cinemas_theaters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CinemasTheatersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
