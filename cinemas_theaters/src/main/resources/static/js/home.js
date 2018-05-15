2a20332478adfd839621024929aaa8adf2a792cd
didulidu <vlada996@gmail.com>
2018-05-16 01:41:55 +0200

    Nesto css zeza...

--- cinemas_theaters/src/main/resources/static/js/home.js
+++ cinemas_theaters/src/main/resources/static/js/home.js
@@ -21,13 +21,6 @@ $(document).ready(function(){
     });
     
 
-$(document).on('click', '.card',function(e){
-    e.preventDefault();
-    var name = this.querySelector(".card-title").innerHTML;
-    var id = this.id.split("theater_id_")[1];
-    localStorage.setItem("theater",id);
-    window.location.replace("profile.html")
-});
 
 
 
