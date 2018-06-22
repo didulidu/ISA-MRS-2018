e7964aeb4f8824d05d20caaf00f3a7517fbc705d
Filip <basarafilip@gmail.com>
2018-06-22 15:49:47 +0200

    promenjeni linkovi na mejlovima

--- src/main/java/com/cinemas_theaters/cinemas_theaters/service/EmailService.java
+++ src/main/java/com/cinemas_theaters/cinemas_theaters/service/EmailService.java
@@ -71,7 +71,7 @@ public class EmailService {
     public  void sendUserActivation(RegisteredUser registeredUser, String token) throws MailException {
         SimpleMailMessage mail = new SimpleMailMessage();
 
-        String activationLink = "https://bioskopi.herokuapp.com/login.html?activate="+token;
+        String activationLink = "https://theatres.herokuapp.com/login.html?activate="+token;
 
         mail.setTo("isamrstest@gmail.com");
         mail.setFrom("isamrs2018@gmail.com");
@@ -107,7 +107,7 @@ public class EmailService {
     public void sendInvitationInfo (RegisteredUser registeredUser, Invitation invitation) throws MailException {
         SimpleMailMessage mail = new SimpleMailMessage();
 
-        String invitationLink = "https://bioskopi.herokuapp.com/registeredUserInvitations.html";
+        String invitationLink = "https://theatres.herokuapp.com/registeredUserInvitations.html";
 
         mail.setTo("isamrstest@gmail.com");
         mail.setFrom("isamrs2018@gmail.com");
